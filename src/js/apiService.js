756c1d628a7987846d73d12807796110eba4d176
Dima Osipov <osipovdima1989@gmail.com>
2020-10-15 14:17:24 +0300

    added succes notification

--- src/js/apiService.js
+++ src/js/apiService.js
@@ -20,6 +20,7 @@ export default {
           notify.noticeAnyMatches();
           return total;
         }
+        notify.noticeSucces();
         this.incrementPage();
         return { hits, total };
       })
--- src/js/notify.js
+++ src/js/notify.js
@@ -3,13 +3,6 @@ import '@pnotify/core/dist/PNotify.css';
 import '@pnotify/core/dist/BrightTheme.css';
 
 export default {
-  notice() {
-    notice({
-      title: 'Regular Notice',
-      text: "Check me out! I'm a notice.",
-      delay: 500,
-    });
-  },
   error(data) {
     error({
       title: 'Oh No!',
